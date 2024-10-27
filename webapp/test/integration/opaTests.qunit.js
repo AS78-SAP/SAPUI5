18984f5a866d359f523f9458a33b0428cf7495a8
AS78-SAP <as.pro@sapo.pt>
2024-10-27 10:46:06 +0000

    First Commit on Development

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["as/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"as/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
